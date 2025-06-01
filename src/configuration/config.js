31f9191a2f1923a80d294d85c7ae4b3f1e0f5fbb
Tejas-Barde <tjbarde30@gmail.com>
2025-06-01 20:28:00 +0530

    All Id's Included

--- src/configuration/config.js
+++ src/configuration/config.js
@@ -1,6 +1,8 @@
-export default config = {
+const config = {
     projectId : String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
     databaseId : String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
     collectionId : String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
     appwriteUrl : String(import.meta.env.VITE_APPWRITE_URL)
 }
+
+export default config;
