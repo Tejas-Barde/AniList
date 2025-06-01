6ff2e3265d0d1a10a64aec0fa4f3516b28632aa1
Tejas-Barde <tjbarde30@gmail.com>
2025-06-01 20:28:53 +0530

    Loader Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,40 @@
-import { useState } from 'react'
 import './App.css'
-import { useForm } from 'react-hook-form';
-import Home from './components/Home';
 import { Outlet } from 'react-router-dom';
+import HeaderComponent from './components/HeaderComponent'
+import FooterComponent from './components/FooterComponent'
+import { useEffect, useState } from 'react';
+import auth from './appwrite/auth';
+import { useDispatch } from 'react-redux';
+import { login, logout } from './store/authSlice';
+import { BiLogoGit } from 'react-icons/bi';
 
 
 function App() {
-  return (
-    <div className='h-fit w-full bg-blue-950'>
-      <Header/>
-      <main className='w-full h-fit'>
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+  
+  useEffect(()=>{
+    try {
+      auth.getUser()
+        .then(response=>{
+          if(response) dispatch(login(response));
+          else dispatch(logout());
+        })
+        .finally(()=>{
+          setLoading(false)
+        })
+    } catch (error) {
+      console.log(`App :: ${error}`);
+    } 
+  },[])  
+
+  return loading ? (<div>Loading</div>) : (
+    <div className='h-full w-full bg-blue-950'>
+      <HeaderComponent/>
+      <main className='w-full h-full'>
         <Outlet/>
       </main>
-      <Footer/>
+      <FooterComponent/>
     </div>
   )
 }
