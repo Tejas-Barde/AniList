58534de3920c1ccd0010f1591daf21740b6f37e5
Tejas-Barde <tjbarde30@gmail.com>
2025-06-02 23:25:57 +0530

    Updated Script 02/06

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -3,7 +3,7 @@ import SignUp from '../components/SignUp'
 
 function SignUpPage() {
   return (
-    <div className='bg-[url(src/assets/anime_background.png)] h-full bg-cover'>
+    <div className='bg-[url(src/assets/anime_background.png)] h-full bg-cover '>
       <SignUp/>
     </div>
   )
