a60f3645c1f82763f22d2087d1f2e95d8360861c
Tejas-Barde <tjbarde30@gmail.com>
2025-06-02 23:27:36 +0530

    New Collection Id Added 02/06

--- src/configuration/config.js
+++ src/configuration/config.js
@@ -2,7 +2,8 @@ const config = {
     projectId : String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
     databaseId : String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
     collectionId : String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
-    appwriteUrl : String(import.meta.env.VITE_APPWRITE_URL)
+    appwriteUrl : String(import.meta.env.VITE_APPWRITE_URL),
+    collection2Id : String(import.meta.env.VITE_APPWRITE_COLLECTION2_ID)
 }
 
 export default config;
