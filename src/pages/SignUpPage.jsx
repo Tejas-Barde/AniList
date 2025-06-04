57931048e6ecce0f949ae1df33bb4531ac0a4662
Tejas-Barde <tjbarde30@gmail.com>
2025-06-04 23:23:42 +0530

    Pages Updated 04/06

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -3,8 +3,13 @@ import SignUp from '../components/SignUp'
 
 function SignUpPage() {
   return (
-    <div className='bg-[url(src/assets/anime_background.png)] h-full bg-cover '>
-      <SignUp/>
+    <div className='relative min-h-screen flex items-center justify-center'>
+      <div className='absolute inset-0 bg-[url("/src/assets/anime_background.png")] bg-cover bg-center z-0' />
+      <div className='absolute inset-0 bg-black opacity-60 z-0' />
+
+      <div className='relative z-10 min-w-100'>
+        <SignUp />
+      </div>
     </div>
   )
 }
