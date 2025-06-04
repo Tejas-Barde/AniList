fae6fa828b8a648ec3f4f714988e8cdd58b788af
Tejas-Barde <tjbarde30@gmail.com>
2025-06-04 23:25:06 +0530

    Sign Up Updated

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -1,66 +1,65 @@
-import { Client , Account, ID} from "appwrite";
+import { Client, Account, ID } from "appwrite";
 import config from '../configuration/config'
 
-class Authentication{
-    client = new Client()
-    account
+class Authentication {
+  client = new Client()
+  account
 
-    constructor(){
-        this.client
-            .setEndpoint(config.appwriteUrl)
-            .setProject(config.projectId)
-        this.account = new Account(this.client)
-    }
+  constructor() {
+    this.client
+      .setEndpoint(config.appwriteUrl)
+      .setProject(config.projectId)
+    this.account = new Account(this.client)
+  }
 
-    async signUp({name,email,password}){
-        try {
-            const userAccount = this.account.create(
-                ID.unique(),
-                email,
-                password,
-                name
-            ).then((response)=>{
-                console.log(response)
-            })
-            return userAccount;
-        } catch (error) {
-            console.log(`Auth :: sign Up :: ${error}`)
-            throw error;
-        }
-        
-    }
-    
-    async login({email,password}){
-        try {
-            console.log(email)
-            const result = await this.account.createEmailPasswordSession(
-                email,
-                password
-            )
-            console.log(result);
-            return result;
-        } catch (error) {
-            console.log(`Auth :: Login :: ${error}`)
-            return null;
-        }
+  async signUp({ name, email, password }) {
+    try {
+      const userAccount = await this.account.create(
+        ID.unique(),
+        email,
+        password,
+        name
+      )
+      return userAccount;
+    } catch (error) {
+      console.log(`Auth :: sign Up :: ${error}`)
+      throw error;
+
     }
 
-    async getUser(){
-        try {
-            return await this.account.get()
-        } catch (error) {
-            console.log(`Auth:: getUser :: ${error}`)
-            return null
-        }
-    }
+  }
 
-    async logout(){
-        try {
-            return await this.account.deleteSession('current')
-        } catch (error) {
-            console.log(`Auth :: LogOut :: ${error}`)
-        }
+  async login({ email, password }) {
+    try {
+      console.log(email)
+      const result = await this.account.createEmailPasswordSession(
+        email,
+        password
+      )
+      console.log(result);
+      return result;
+    } catch (error) {
+      console.log(`Auth :: Login :: ${error}`)
+      return null;
     }
+  }
+
+  async getUser() {
+    try {
+      return await this.account.get()
+    } catch (error) {
+      console.log(`Auth:: getUser :: ${error}`)
+      return null
+    }
+  }
+
+  async logout() {
+    try {
+      return await this.account.deleteSession('current')
+    } catch (error) {
+      console.log(`Auth :: LogOut :: ${error}`)
+    }
+  }
 }
 
 const authentication = new Authentication();
