83513a767715462f62b11e20aafb9b3448ea55ca
Tejas-Barde <tjbarde30@gmail.com>
2025-06-05 20:04:38 +0530

    update and remove fun added

--- src/store/animeSlice.js
+++ src/store/animeSlice.js
@@ -15,13 +15,33 @@ const animeSlice = createSlice({
         },
         fillUserList:(state,action)=>{
             state.userList = action.payload
+            state.userList.sort((a,b)=> a.anime_id - b.anime_id)
         },
-        addToUserList(state,action){
+        addToUserList:(state,action)=>{
+            state.userList.sort((a,b)=> a.anime_id - b.anime_id)
             state.userList.push(action.payload)
+        },
+        removeFromUserList:(state,action)=>{
+            state.userList = state.userList.filter(anime => anime.anime_id !== action.payload)
+        },
+        updateUserList:(state,action)=>{
+            let left = 0;
+            let right = state.userList.length - 1;
+            while(left <= right){
+                const mid = Math.floor((left + right) / 2);
+                const midId = state.userList[mid].anime_id;
+                if(midId < action.payload.anime_id) left = mid + 1;
+                else if(midId > action.payload.anime_id) right = mid - 1;
+                else {
+                    const anime = state.userList[mid];
+                    anime.rating = action.payload.rating;
+                    anime.status = action.payload.status;
+                    return;
+                }
+            }
         }
-
     }
 })
 
-export const {fillTrendingList,fillUserList,addToUserList} = animeSlice.actions;
+export const {fillTrendingList,fillUserList,addToUserList,removeFromUserList,updateUserList} = animeSlice.actions;
 export default animeSlice.reducer; 
