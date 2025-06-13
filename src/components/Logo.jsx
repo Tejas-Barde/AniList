9583d78abb7f81fdb5635ef4b3d620e8a29cecab
Tejas-Barde <tjbarde30@gmail.com>
2025-06-14 00:00:38 +0530

    Updated  13/06

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function Logo(...props) {
+function Logo({...props}) {
   return (
     <img className='w-10 h-auto rounded-full' src='src\assets\logo.jpg'{...props} />
   )
