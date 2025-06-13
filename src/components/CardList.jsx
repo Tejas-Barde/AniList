01a8c81decbab8d19034f783da4e9743dddc4985
Tejas-Barde <tjbarde30@gmail.com>
2025-06-13 23:58:50 +0530

    Updated  13/06

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -5,22 +5,30 @@ import { Link } from 'react-router-dom';
 
 function CardList({ animeList = null, userCard = false, status = "all" }) {
   if (animeList === null) animeList = useSelector((state) => state.anime.trendingList);
-  console.log(`CardList :: status :: ${status}`);
+  // console.log(`CardList :: status :: ${status}`);
   return (
-    <ul className="flex p-6 gap-x-12 gap-4 flex-wrap start-left w-full">
-      {animeList.map((anime, index) => (
-          <li key={index}>
-            {userCard ?
-              (status === 'all' || anime.status.toLowerCase() === status) &&
-              (
+    <ul className="flex p-4 gap-x-12 gap-4 flex-wrap start-left w-full">
+      {animeList.map((anime, index) =>{
+        if(userCard){
+          if(status === 'all' || anime.status.toLowerCase() === status) {
+            return (
+              <li key={index}>
                 <UserCard anime={anime} index={index} />
-              ) : (
-                <Link to={`/search/${anime.title}`}>
-                  <Card anime={anime} />
-                </Link>
-              )}
-          </li>
-      ))}
+              </li>
+            );
+          }
+        }
+        else{
+          return(
+            <li key={index}>
+              <Link to={`/search/${anime.title}`}>
+                <Card anime={anime} />
+              </Link>
+            </li>
+          )
+        }
+      } 
+      )}
     </ul>
   );
 }
