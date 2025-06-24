807857fc02ccf9c0c79d6c17777c98b6b9978be8
Tejas-Barde <tjbarde30@gmail.com>
2025-06-24 18:39:09 +0530

    Updated 24/06

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -2,23 +2,37 @@ import React, { useEffect, useState } from 'react'
 import EditDetails from './EditDetails'
 import { FiMoreVertical } from 'react-icons/fi'
 import Button from './Button'
+import { Link, useNavigate } from 'react-router-dom'
 
 
-const UserCard = React.memo(({anime,index})=>{
+const UserCard = React.memo(({ anime, index }) => {
   // console.log(`UserCard :: Anime`)
   // console.log(anime)
   const [edit, setEdit] = useState(false);
+  const navigate = useNavigate();
+  const handleCardClick = ()=>{
+    navigate(`/search/${anime.title}`)
+  }
   return (
     <>
-    
-      <div className="flex gap-5 w-80 h-[214px] bg-gray-900 text-white p-2 rounded-md items-stretch">
+      <div 
+        onClick={handleCardClick}
+        className="cursor-pointer flex z-0 gap-5 w-80 h-[214px] bg-gray-900 text-white rounded-md items-stretch
+       border-gray-800 border-2
+            shadow-gray-800/50 shadow-[0_0_10px_rgba(59,130,246,0.5)]
+             hover:border-blue-500 hover:shadow-[0_0_8px_2px_rgba(59,130,246,0.5)] 
+             transition duration-300 ease-in-out overflow-hidden p-2">
         <div className='w-full h-full'>
           <img className='w-auto h-full ' src={anime.image_url} alt="image" />
         </div>
         <div className="w-full flex flex-col gap-2 justify-between align-middle text-[10px]">
           <div className="flex gap-2 justify-between items-start align-baseline">
             <span className="bg-gray-600 px-2 py-0.5 rounded-full text-white">{anime.status}</span>
-            <Button onClick={() => setEdit(true)}  className="p-1"  >
+            <Button onClick={(e) => {
+                e.stopPropagation();
+                setEdit(true)
+              }
+            } className="p-1 z-10 hover:border-1 hover:bg-gray-700 rounded-xl ">
               <FiMoreVertical></FiMoreVertical>
             </Button>
           </div>
@@ -28,7 +42,7 @@ const UserCard = React.memo(({anime,index})=>{
           <div className='flex min-h-[3.5em] items-center'>
             <p className="font-bold text-[1.5em]  line-clamp-2">{anime.title}</p>
           </div>
-    
+
           <div className="flex justify-between items-center">
             <div className="flex items-center gap-1">
               <svg viewBox="0 0 24 24" className="w-3 h-3 fill-yellow-400" xmlns="http://www.w3.org/2000/svg">
@@ -37,7 +51,7 @@ const UserCard = React.memo(({anime,index})=>{
               <span>{anime.rating}</span>
             </div>
           </div>
-    
+
           <div className="flex gap-1 flex-wrap">
             {anime?.genres?.slice(0, 2).map((genre, index) => (
               <span className='border-1 border-solid bg-auto border-sky-800 rounded p-1.5' key={index}>{genre}</span>
@@ -46,12 +60,14 @@ const UserCard = React.memo(({anime,index})=>{
         </div>
       </div>
       {edit && (
-  <EditDetails
-    anime={anime}
-    index={index}
-    onClose={() => setEdit(false)}
-  />
-)}
+        <EditDetails
+          anime={anime}
+          index={index}
+          onClose={(e) => {
+            setEdit(false);
+          }}
+        />
+      )}
 
     </>
   )
