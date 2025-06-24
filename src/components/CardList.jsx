4f5cb4f9571e8db7868629888218406b3a641400
Tejas-Barde <tjbarde30@gmail.com>
2025-06-24 18:38:17 +0530

    Updated

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -3,14 +3,14 @@ import { useSelector } from 'react-redux';
 import UserCard from './UserCard';
 import { Link } from 'react-router-dom';
 
-function CardList({ animeList = null, userCard = false, status = "all" }) {
+function CardList({ animeList = null, userCard = false, status = "all", className = "" }) {
   if (animeList === null) animeList = useSelector((state) => state.anime.trendingList);
   // console.log(`CardList :: status :: ${status}`);
   return (
-    <ul className="flex p-4 gap-x-12 gap-4 flex-wrap start-left w-full">
-      {animeList.map((anime, index) =>{
-        if(userCard){
-          if(status === 'all' || anime.status.toLowerCase() === status) {
+    <ul className={`flex p-4 flex-wrap gap-2 sm:gap-4 md:gap-10 lg:gap-8 w-full ${className}`}>
+      {animeList.map((anime, index) => {
+        if (userCard) {
+          if (status === 'all' || anime.status.toLowerCase() === status) {
             return (
               <li key={index}>
                 <UserCard anime={anime} index={index} />
@@ -18,8 +18,8 @@ function CardList({ animeList = null, userCard = false, status = "all" }) {
             );
           }
         }
-        else{
-          return(
+        else {
+          return (
             <li key={index}>
               <Link to={`/search/${anime.title}`}>
                 <Card anime={anime} />
@@ -27,7 +27,7 @@ function CardList({ animeList = null, userCard = false, status = "all" }) {
             </li>
           )
         }
-      } 
+      }
       )}
     </ul>
   );
