3711f8562aa9e444909a511c58e760e4a26256db
Tejas-Barde <tjbarde30@gmail.com>
2025-06-24 18:39:32 +0530

    Updated Login Page 24/06

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import Login from '../components/Login'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 
 function LoginPage() {
+  const authStatus = useSelector((state) => state.auth.status)
+  const navigate = useNavigate()
+  if(authStatus) navigate('/') 
   return (
     <div className='relative min-h-screen flex items-center justify-center'>
       <div className='absolute inset-0 bg-[url("/src/assets/anime_background.png")] bg-cover bg-center z-0' />
