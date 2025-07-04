4c60ec3d669734447d16dd3269397ed070fcbdf6
Tejas-Barde <tjbarde30@gmail.com>
2025-07-05 00:21:49 +0530

    Updated 05/07

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -28,7 +28,7 @@ function SearchPage() {
   return loading ? (
     <Loader className='text-white'></Loader>
   ) : (
-    <div className="pt-20 flex flex-col gap-4 w-full mb-4 bg-gradient-to-br from-[#0A121D] via-[#131C2B] to-[#1B2A3C]">
+    <div className="pt-20 flex flex-col gap-4 w-full mb-4 bg-gradient-to-br from-[#0A121D] via-[#131C2B] to-[#1B2A3C] text-white">
       {animeList.length > 0 && <LargeCard anime={animeList[0]} />}
       {animeList.length > 0 && (
         <div >
@@ -36,7 +36,7 @@ function SearchPage() {
             <div className="w-1 h-6 bg-red-500 rounded-sm" />
             <h1 className="text-xl md:text-2xl font-bold text-amber-100 uppercase tracking-wide">Related Search</h1>
           </div>
-          <CardList animeList={animeList.slice(1)} className='pl-[5em]'/>
+          <CardList animeList={animeList.slice(1)} />
         </div>
       )}
       {animeList.length === 0 && (
